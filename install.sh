0d2cb99aea51bce2c2753d989387ad975f9f3681
Daniil Polyakov <arjentix@gmail.com>
2022-11-07 18:06:03 +0300

    Add .zshrc

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 
-NVIM=~/.config/nvim
+install_zshrc() {
+    ZSH=~/.zshrc
 
-if [ -d $NVIM ];
-then
-    cp $NVIM ~/.config/nvim_backup
-    rm -rf $NVIM
-fi
-ln -s $(pwd)/nvim $NVIM
+    if [ -f $ZSH ];
+    then
+        cp $ZSH ~/.zshrc_backup
+        rm -rf $ZSH
+    fi
+    ln -s $(pwd)/.zshrc $ZSH
+}
+
+install_nvim() {
+    NVIM=~/.config/nvim
+
+    if [ -d $NVIM ];
+    then
+        cp $NVIM ~/.config/nvim_backup
+        rm -rf $NVIM
+    fi
+    ln -s $(pwd)/nvim $NVIM
+}
+
+install_zshrc
+install_nvim
 
